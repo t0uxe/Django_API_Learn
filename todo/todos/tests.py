98b2388b566a0f3329316d398bf97828f0b9f22d
t0uxe <t0uxe@tuta.io>
2022-04-08 13:09:00 +0200

    Actualizaciones para despliegue en Heroku

---
+++ todo/todos/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="Todo 1",
+            body="El cuerpo del Todo"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "Todo 1")
+        self.assertEqual(self.todo.body, "El cuerpo del Todo")
+        self.assertEqual(str(self.todo), "Todo 1")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Todo 1")
