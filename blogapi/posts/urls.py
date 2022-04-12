a0140ac92c0491d3079f25e6e17ea9029796ae43
t0uxe <t0uxe@tuta.io>
2022-04-12 15:51:28 +0200

    Añadidos permisos

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -4,6 +4,6 @@ from .views import PostList, PostDetail
 
 
 urlpatterns = [
-    path("/<int:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("", PostList.as_view(), name="post_list"),
 ]
