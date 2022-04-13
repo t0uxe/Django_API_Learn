9b872d9fc93eb88cdf6eeff151335b268c44f038
t0uxe <t0uxe@tuta.io>
2022-04-13 12:54:34 +0200

    Añadida configuración de autenticación por token

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -37,15 +37,32 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # Terceros
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    # Local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
 ]
 
 REST_FRAMEWORK = {
+    # Autorización
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    # Autenticación
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        # Autenticación básica
+        #"rest_framework.authentication.BasicAuthentication",
     ],
 }
 
@@ -80,10 +97,14 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SITE_ID = 1
 
 WSGI_APPLICATION = "django_project.wsgi.application"
 
