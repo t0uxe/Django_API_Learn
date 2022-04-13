0651687e21d2cda0ba23e425a992d2872228147a
t0uxe <t0uxe@tuta.io>
2022-04-13 14:28:39 +0200

    Añadidos Viewsets y Routers

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,8 +1,27 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+#from rest_framework import generics, permissions
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+""" 
+En lugar de hacer vistas individuales para listar y el detalle,
+se pueden agrupar en ViewSets como se ve arriba.
 
 
 class PostList(generics.ListCreateAPIView):
@@ -15,3 +34,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+ """
