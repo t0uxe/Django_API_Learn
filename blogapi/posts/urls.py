2171c3d92377b8af5f4ab5313d4c5ae482cdea9d
t0uxe <t0uxe@tuta.io>
2022-04-13 14:28:39 +0200

    Añadido schema y documentación

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+#from .views import PostList, PostDetail, UserList, UserDetail
+from .views import UserViewSet, PostViewSet
 
-from .views import PostList, PostDetail
 
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+urlpatterns = router.urls
+
+""" 
+Aquí abajo es como lo hacía antes.
+Ahora los Routers nos generan los urls de manera automática.
 
 urlpatterns = [
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("", PostList.as_view(), name="post_list"),
 ]
+ """
